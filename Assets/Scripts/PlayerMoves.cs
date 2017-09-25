d0b173d554b85f9bb354209fdc1b8c35a698e781
OTAKU <otaku.suprem@gmail.com>
2017-09-25 16:29:36 +0200

    Prefab player update

---
+++ Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMoves : NetworkBehaviour {
+
+	public GameObject playerCamera ;
+
+	public override void OnStartLocalPlayer ()
+	{
+		gameObject.tag = "Player" ;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public override void OnStartClient ()
+	{
+		if(!isLocalPlayer)
+		{
+			playerCamera.SetActive(false) ;
+		}
+	}
+}
