e46fbfb12219f51cb417ef87ed91853a860fade2
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-09-26 10:40:28 +0200

    Identity

--- Assets/Scripts/Player_Manager.cs
+++ Assets/Scripts/Player_Manager.cs
@@ -30,50 +30,5 @@ public class Player_Manager : NetworkBehaviour  {
 	{	
 		transform.position = new Vector3(0,0,0) ;
 	}
-	/*void OnTriggerEnter (Collider other)
-	{
-
-		if (other.CompareTag("Checkpoint"))
-		{
-			currentCheckpointPosition = other.transform.position;
-			Debug.Log("Checkpoint");
-			other.gameObject.SetActive(false) ;
-
-		}
-		
-		if (other.CompareTag("DeathTrigger"))
-		{
-			transform.position = currentCheckpointPosition;
-			Debug.Log("Mort");
-			GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
-			
-		}
-		if (other.CompareTag("WinTrigger"))
-		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-			Debug.Log("Win");
-		}
-		if (other.CompareTag("TutoMoves"))
-		{
-			TutoMoves.SetActive(true);
-		}
-		if (other.CompareTag("TutoJump"))
-		{
-			TutoJump.SetActive(true);
-		}
-
-	}
-	void OnTriggerExit (Collider other)
-	{
-		if (other.CompareTag("TutoMoves"))
-		{
-			TutoMoves.SetActive(false);
-		}
-		if (other.CompareTag("TutoJump"))
-		{
-			TutoJump.SetActive(false);
-		}
-	}*/
-
 
 }
