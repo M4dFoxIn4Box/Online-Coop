d24bed922030bf353935958f5c998c8884a1c545
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-09-29 13:22:14 +0200

    Lobby player WIP

---
+++ Assets/Scripts/Player_scripts/MyLobbyPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyLobbyPlayer : NetworkLobbyPlayer {
+
+	public override void OnStartLocalPlayer (){
+		DontDestroyOnLoad(gameObject);
+		StartCoroutine("Ready");
+	}
+
+	void Update () {
+
+	}
+
+	public IEnumerator Ready (){
+		yield return new WaitForSeconds(0.3f);
+		SendReadyToBeginMessage();
+	}
+
+	public override void OnClientReady (bool readyState){
+		if (!readyState){
+			SendSceneLoadedMessage();
+		}
+	}
+}
