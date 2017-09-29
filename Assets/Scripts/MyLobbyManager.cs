8248e831a220198fd5d871df4edf100cbe4ae160
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-09-29 13:14:53 +0200

    UI Lobby WIP

--- Assets/Scripts/MyLobbyManager.cs
+++ Assets/Scripts/MyLobbyManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 public class MyLobbyManager : NetworkLobbyManager  {
 
@@ -13,6 +14,9 @@ public class MyLobbyManager : NetworkLobbyManager  {
 	public List<Player_Camera> playerList = new List<Player_Camera>();
 
 	private int randomIndex ;
+
+	private string createMatchPwd = "";
+	private string joinMatchPwd = "";
 
 	private static MyLobbyManager instance;
     public static MyLobbyManager Instance ()
@@ -120,5 +124,58 @@ public class MyLobbyManager : NetworkLobbyManager  {
 		yield return new WaitForSeconds (0.8f);
 		playerList[0].RpcGoTo(startPositions[0].gameObject);
 		playerList[1].RpcGoTo(startPositions[1].gameObject);
+	}
+
+	//-------------------------------------------//
+
+	public void OnClickStart (int playersNb){
+		minPlayers = playersNb;
+		SearchForGame();	// looks for a match the player can join, or else creates one
+	}
+
+	public void SearchForGame (){
+		StartMatchMaker();
+		matchMaker.ListMatches(0, 10, "ROOL", false, 0, 0, OnMatchList);
+	}
+
+	public override void OnMatchList (bool succes, string extendedInf, List<MatchInfoSnapshot> matchList){
+		if (LogFilter.logDebug){
+			Debug.Log("NetworkManager OnMatchList ");
+		}
+        matches = matchList;
+        // If the player can join a match, joins it...
+        for (var i = 0; i < matches.Count; i++){
+			if (matches[i].currentSize < minPlayers){
+				matchMaker.JoinMatch(matches[i].networkId, joinMatchPwd," ", " ", 0, 0, OnMatchJoined);
+				return;
+			}
+		}
+		// ... or else creates a match
+		CreateMatch ();
+	}
+
+	public override void OnMatchJoined (bool success, string extendedInf, MatchInfo matchInfo){
+		if (LogFilter.logDebug){
+			Debug.Log("NetworkManager OnMatchJoined ");
+		}
+        if (success){
+        	Utility.SetAccessTokenForNetwork(matchInfo.networkId, matchInfo.accessToken);
+			StartClient(matchInfo);
+        }
+        else {
+            if (LogFilter.logError) {
+            	Debug.LogError("Join Failed:" + matchInfo);
+            }
+        }
+	}
+
+	public void CreateMatch (){
+		matchMaker.CreateMatch("ROOL", 2, true, createMatchPwd, " ", " ", 0, 0, OnMatchCreate);
+	}
+
+	public override void OnMatchCreate (bool success, string extendedInfo, MatchInfo matchInfo){
+		if (success) {
+			StartHost(matchInfo);
+		}
 	}
 }
