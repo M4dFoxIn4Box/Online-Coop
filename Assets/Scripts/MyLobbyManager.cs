e0bd8ed765710970a93d0303f601c003189af4cf
DESKTOP-92OCKSC\Kevin <ibanez13keke@gmail.com>
2017-09-29 11:33:51 +0200

    *.0*

--- Assets/Scripts/MyLobbyManager.cs
+++ Assets/Scripts/MyLobbyManager.cs
@@ -73,8 +73,11 @@ public class MyLobbyManager : NetworkLobbyManager  {
 		Debug.Log(nbToWin);
 		if (nbToWin >= 2)
 		{
-			currentSceneIndex++ ;
-			NetworkLobbyManager.singleton.ServerChangeScene(sceneBoard[currentSceneIndex]);
+			if(sceneBoard[currentSceneIndex+1] != null)
+			{
+				currentSceneIndex++ ;
+				NetworkLobbyManager.singleton.ServerChangeScene(sceneBoard[currentSceneIndex]);
+			}
 		}
 	}
 
